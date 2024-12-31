78c3274ee9b5784edccb845ccd2dbe161bc087d4
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2024-12-31 12:28:38 -0600

    adding CORS

--- src/API/app.py
+++ src/API/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request  # type: ignore
 from flask_mysqldb import MySQL  # type: ignore
 import os
+from flask_cors import CORS  # type: ignore
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 # MySQL configurations - using your provided credentials
 # (but please change these ASAP for security!)
