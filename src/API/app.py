7fccdeb05b00b0a609da67aa03dec53d83339f3a
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-01-01 16:59:54 -0600

    updating CORS policy to allow any ip address formatted domain for the ESPs

--- src/API/app.py
+++ src/API/app.py
@@ -38,7 +38,11 @@ CORS(
         r"/*": {
             "origins": "*",
             "methods": ["GET", "POST", "OPTIONS"],
-            "allow_headers": ["Content-Type", "Authorization"],
+            "allow_headers": [
+                "Content-Type",
+                "Authorization",
+                "Access-Control-Allow-Credentials",
+            ],  # Added Access-Control-Allow-Credentials
             "expose_headers": ["Content-Type", "Authorization", "Set-Cookie"],
             "supports_credentials": True,
         }
@@ -63,7 +67,9 @@ def after_request(response):
     if origin and allowed_origin(origin):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Headers"] = (
+            "Content-Type, Authorization, Access-Control-Allow-Credentials"  # Added Access-Control-Allow-Credentials
+        )
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
 
     return response
@@ -275,8 +281,9 @@ def login():
             response.headers["Access-Control-Allow-Origin"] = origin
             response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
             response.headers["Access-Control-Allow-Headers"] = (
-                "Content-Type, Authorization"
+                "Content-Type, Authorization, Access-Control-Allow-Credentials"  # Added Access-Control-Allow-Credentials
             )
+
             response.headers["Access-Control-Allow-Credentials"] = "true"
         return response
 
