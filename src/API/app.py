f2984a7326543ac03e04149867a6c7bc29c14aa6
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-01-02 00:52:25 -0600

    updating CORS policy to allow any ip address formatted domain for the ESPs

--- src/API/app.py
+++ src/API/app.py
@@ -215,7 +215,7 @@ def new_plant_node():
 
         # Retrieve userUUID from the database
         user_query = """
-            SELECT userUUID FROM automatic_watering_system.users WHERE username = '%s'
+            SELECT userUUID FROM automatic_watering_system.users WHERE username = `%s`
         """
         logging.info(f"userUUID Query: {user_query % username}")
         cur = mysql.connection.cursor()
