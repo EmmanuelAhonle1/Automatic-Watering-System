45c58b634459d2a77dd7fe87274b60b83679f33f
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-01-02 00:37:49 -0600

    updating CORS policy to allow any ip address formatted domain for the ESPs

--- src/API/app.py
+++ src/API/app.py
@@ -217,7 +217,7 @@ def new_plant_node():
         user_query = """
             SELECT userUUID FROM users WHERE username = %s
         """
-        logging.info(f"userUUID Query: {user_query, (username,)})")
+        logging.info(f"userUUID Query: {user_query % username})")
         cur = mysql.connection.cursor()
         cur.execute(user_query, (username,))
         user = cur.fetchone()
@@ -234,7 +234,7 @@ def new_plant_node():
             WHERE connectedUserUUID = %s AND nodeName = %s
         """
 
-        logging.info(f"Check Query: {check_query, (user_uuid, node_name)}")
+        logging.info(f"Check Query: {check_query % user_uuid, node_name}")
         cur = mysql.connection.cursor()
         cur.execute(check_query, (user_uuid, node_name))
         existing_plant = cur.fetchone()
@@ -261,7 +261,7 @@ def new_plant_node():
             moisture_threshold_id,
             temperature_threshold_id,
         )
-        logging.info("Query: {}".format((query, params)))
+        logging.info(f"Final Query: {query % params}")
 
         cur.execute(query, params)
         mysql.connection.commit()
