7e214d0a143120e33c9f9448f7273b1c39bc70f0
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-01-28 15:01:10 -0600

    adding all setup files

---
+++ src/Frontend/aws-frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import PlantCard from "./components/PlantCard/PlantCard";
+
+const App = () => {
+  const plantData = {
+    name: "Fern",
+    species: "Adiantum",
+    wateringFrequency: "Every 3 days",
+    soilMoisture: "45%",
+    lastWatered: "2023-10-01",
+    location: "Living Room",
+    sunlightRequirements: "Partial Shade",
+    temperature: "20°C",
+    humidity: "70%",
+  };
+
+  return (
+    <div>
+      <h1 id="awsTitle">Automatic Watering System</h1>
+      <div id="card-container" className="card-container">
+        <PlantCard plant={plantData} />
+        <PlantCard plant={plantData} />
+        <PlantCard plant={plantData} />
+      </div>
+    </div>
+  );
+};
+
+// Create root and render
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+export default App;
---
+++ src/Frontend/aws-frontend/src/components/PlantCard/PlantCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./plantCard.css";
+
+const PlantCard = ({ plant }) => {
+  return (
+    <div className="card">
+      <h2>{plant.name}</h2>
+      <p>Species: {plant.species}</p>
+      <p>Watering Frequency: {plant.wateringFrequency}</p>
+      <p>Soil Moisture: {plant.soilMoisture}</p>
+      <p>Last Watered: {plant.lastWatered}</p>
+      <p>Location: {plant.location}</p>
+      <p>Sunlight Requirements: {plant.sunlightRequirements}</p>
+      <p>Temperature: {plant.temperature}</p>
+      <p>Humidity: {plant.humidity}</p>
+    </div>
+  );
+};
+
+export default PlantCard;
