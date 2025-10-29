05a5630d91007c0dec23fdc8ec5fa8561ff630e2
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-10-28 21:33:43 -0500

    disabling Sidebar

--- src/Frontend/aws-frontend/src/App.jsx
+++ src/Frontend/aws-frontend/src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "./styles.css";
 import PlantCardContainer from "./components/Dashboard/CardContainer/CardContainer";
 import DashboardHeader from "./components/Dashboard/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
+// Temporarily disabled Sidebar import. To re-enable, uncomment the line below.
+// import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
       <DashboardHeader />
       <PlantCardContainer />
+      {/* Sidebar temporarily disabled. To re-enable, uncomment the import above and this component:
       <Sidebar />
+    */}
     </div>
   );
 };
